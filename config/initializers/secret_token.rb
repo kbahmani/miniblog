5c0fe2d64dc8894b7f1f45ae6c18c0e50e50e32f
kia <kbahmani@mudynamics.com>
2011-11-15 14:08:43 -0800

    start miniblog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniblog::Application.config.secret_token = '78e11f9569f027ee9540dcd7e736ea1cc0123ac615a671a1f63c865540664c9f0627ebca4c81d4058fa137e943183e13b69487ea3be803e627d61f90a7d470cc'
